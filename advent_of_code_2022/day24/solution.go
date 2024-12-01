8daf51a48995dd5ed5f429f24789fb8ca07cb951
DmitryTsepelev <dmitry.a.tsepelev@gmail.com>
2024-12-01 23:31:19 +0300

    2022 day24

--- advent_of_code_2022/day24/solution.go
+++ advent_of_code_2022/day24/solution.go
@@ -19,13 +19,14 @@ type Blizzard struct {
 	dir rune
 }
 
-func getInputData() ([]Blizzard, int, int) {
+func getInputData() ([]*Blizzard, map[Point]bool, int, int) {
 	file, _ := os.Open("./input.txt")
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
 
-	blizzards := make([]Blizzard, 0)
+	blizzards := make([]*Blizzard, 0)
+	wallMap := map[Point]bool{}
 
 	row := 0
 	col := 0
@@ -35,201 +36,150 @@ func getInputData() ([]Blizzard, int, int) {
 		col = len(line)
 
 		for col, dir := range line {
-			if dir == '.' || dir == '#' {
+			switch dir {
+			case '.':
 				continue
+			case '#':
+				wallMap[Point{row, col}] = true
+			default:
+				blizzard := Blizzard{row: row, col: col, dir: dir}
+
+				blizzards = append(blizzards, &blizzard)
 			}
-
-			blizzard := Blizzard{row: row, col: col}
-
-			blizzard.dir = dir
-
-			blizzards = append(blizzards, blizzard)
 		}
 
 		row++
 	}
 
-	return blizzards, row - 1, col - 1
+	return blizzards, wallMap, row - 1, col - 1
 }
 
-func moveBlizzards(blizzards []Blizzard, fieldHeight int, fieldWidth int) []Blizzard {
-	newBlizzards := make([]Blizzard, len(blizzards))
-
-	for idx, blizzard := range blizzards {
-		newBlizzard := Blizzard{
-			row: blizzard.row,
-			col: blizzard.col,
-			dir: blizzard.dir,
-		}
-
-		switch newBlizzard.dir {
+func moveBlizzards(blizzards []*Blizzard, wallMap map[Point]bool, fieldHeight int, fieldWidth int) {
+	for _, blizzard := range blizzards {
+		switch blizzard.dir {
 		case RIGHT:
-			newBlizzard.col++
-			if newBlizzard.col == fieldWidth {
-				newBlizzard.col = 1
+			blizzard.col++
+			if _, ok := wallMap[Point{blizzard.row, blizzard.col}]; ok {
+				blizzard.col = 1
 			}
 		case LEFT:
-			newBlizzard.col--
-			if newBlizzard.col == 0 {
-				newBlizzard.col = fieldWidth - 1
+			blizzard.col--
+			if _, ok := wallMap[Point{blizzard.row, blizzard.col}]; ok {
+				blizzard.col = fieldWidth - 1
 			}
 		case DOWN:
-			newBlizzard.row++
-			if newBlizzard.row == fieldHeight {
-				newBlizzard.row = 1
+			blizzard.row++
+			if _, ok := wallMap[Point{blizzard.row, blizzard.col}]; ok {
+				blizzard.row = 1
 			}
 		case UP:
-			newBlizzard.row--
-			if newBlizzard.row == 0 {
-				newBlizzard.row = fieldHeight - 1
+			blizzard.row--
+			if _, ok := wallMap[Point{blizzard.row, blizzard.col}]; ok {
+				blizzard.row = fieldHeight - 1
 			}
 		}
-
-		newBlizzards[idx] = newBlizzard
-	}
-
-	return newBlizzards
-}
-
-func printField(blizzards []Blizzard, fieldHeight int, fieldWidth int) {
-	for row := 0; row <= fieldHeight; row++ {
-		for col := 0; col <= fieldWidth; col++ {
-			if (row == 0 && col != 1) || (row == fieldHeight && col != fieldWidth-1) || col == 0 || col == fieldWidth {
-				fmt.Print("#")
-			} else {
-				found := make([]rune, 0)
-				for _, blizzard := range blizzards {
-					if blizzard.row == row && blizzard.col == col {
-						found = append(found, blizzard.dir)
-					}
-				}
-
-				if len(found) == 0 {
-					fmt.Print(".")
-				} else if len(found) == 1 {
-					fmt.Print(string(found[0]))
-				} else {
-					fmt.Print(len(found))
-				}
-			}
-		}
-
-		fmt.Println()
 	}
 }
-
-// func findLoop(blizzards []Blizzard, fieldHeight int, fieldWidth int) int {
-// 	current := blizzards
-
-// 	idx := 0
-// 	for {
-// 		current = moveBlizzards(current, fieldHeight, fieldWidth)
-// 		if reflect.DeepEqual(blizzards, current) {
-// 			return idx
-// 		}
-// 		idx++
-// 	}
-// }
 
 type Point struct {
 	row int
 	col int
 }
 
-func bfs(blizzards []Blizzard, fieldHeight int, fieldWidth int) int {
-	queue := map[Point]bool{
-		{row: 0, col: 1}: true,
+type Elf struct {
+	point Point
+	stage Stage
+}
+
+var elfDeltas = []Point{
+	{0, 0},
+	{-1, 0},
+	{1, 0},
+	{0, 1},
+	{0, -1},
+}
+
+func validElf(elfPoint Point, fieldHeight, fieldWidth int, blizzardMap map[Point]bool, wallMap map[Point]bool) bool {
+	if elfPoint.row < 0 || elfPoint.row > fieldHeight || elfPoint.col < 0 || elfPoint.col > fieldWidth {
+		return false
 	}
-	// printField(blizzards, fieldHeight, fieldWidth)
-	// fmt.Println()
 
-	minute := 0
+	if _, ok := blizzardMap[elfPoint]; ok {
+		return false
+	}
+
+	if _, ok := wallMap[elfPoint]; ok {
+		return false
+	}
+
+	return true
+}
+
+type Stage = int
+
+const FIRST_FORWARD = 0
+const BACK = 1
+const SECOND_FORWARD = 2
+
+func reachedStartPoint(elf Point) bool {
+	return elf.row == 0 && elf.col == 1
+}
+
+func reachedFinishPoint(elf Point, fieldHeight, fieldWidth int) bool {
+	return elf.row == fieldHeight && elf.col == fieldWidth-1
+}
+
+func findPath(blizzards []*Blizzard, wallMap map[Point]bool, fieldHeight int, fieldWidth int, withReturn bool) int {
+	elves := map[Elf]bool{
+		{Point{0, 1}, FIRST_FORWARD}: true,
+	}
+
+	minute := 1
+
 	for {
-		minute++
+		moveBlizzards(blizzards, wallMap, fieldHeight, fieldWidth)
 
-		// fmt.Println("minute", minute)
+		nextElves := map[Elf]bool{}
 
-		nextQueue := make(map[Point]bool, 0)
-		// fmt.Println("Before", len(blizzards))
-		blizzards = moveBlizzards(blizzards, fieldHeight, fieldWidth)
-		// fmt.Println("after", len(blizzards))
-		// printField(blizzards, fieldHeight, fieldWidth)
-
-		for point := range queue {
-			// fmt.Println(fieldHeight, fieldWidth)
-			if point.row == fieldHeight && point.col == fieldWidth-1 {
-				return minute
-			}
-			canStay := true
-			canGoLeft := point.row > 0 && point.col > 1
-			canGoRight := point.row > 0 && point.col < fieldWidth-1
-			canGoUp := point.row > 1
-			canGoDown := point.row < fieldHeight-1 || point.col == fieldWidth-1
-			// fmt.Println("fieldHeight", fieldHeight)
-			// fmt.Println(point, canStay, canGoLeft, canGoRight, canGoUp, canGoDown)
-			for _, nextBlizzard := range blizzards {
-				if point.row == nextBlizzard.row && point.col == nextBlizzard.col {
-					canStay = false
-				}
-
-				if canGoUp && point.row-1 == nextBlizzard.row && point.col == nextBlizzard.col {
-					canGoUp = false
-				}
-
-				if canGoDown && point.row+1 == nextBlizzard.row && point.col == nextBlizzard.col {
-					canGoDown = false
-				}
-
-				if canGoLeft && point.row == nextBlizzard.row && point.col-1 == nextBlizzard.col {
-					canGoLeft = false
-				}
-
-				if canGoRight && point.row == nextBlizzard.row && point.col+1 == nextBlizzard.col {
-					canGoRight = false
-				}
-			}
-			// fmt.Println(point, "canStay =", canStay, "canGoLeft =", canGoLeft, "canGoRight =", canGoRight, "canGoUp =", canGoUp, "canGoDown =", canGoDown)
-
-			// fmt.Println(point)
-			if canStay {
-				// fmt.Println("canStay")
-				nextQueue[point] = true
-			}
-
-			if canGoLeft {
-				// fmt.Println("canGoLeft")
-				nextQueue[Point{row: point.row, col: point.col - 1}] = true
-			}
-
-			if canGoRight {
-				// fmt.Println("canGoRight")
-				nextQueue[Point{row: point.row, col: point.col + 1}] = true
-			}
-
-			if canGoUp {
-				// fmt.Println("canGoUp")
-				nextQueue[Point{row: point.row - 1, col: point.col}] = true
-			}
-
-			if canGoDown {
-				// fmt.Println("canGoDown")
-				nextQueue[Point{row: point.row + 1, col: point.col}] = true
-			}
-			// fmt.Println()
+		blizzardMap := map[Point]bool{}
+		for _, blizzard := range blizzards {
+			blizzardMap[Point{blizzard.row, blizzard.col}] = true
 		}
 
-		queue = nextQueue
+		for elf := range elves {
+			for _, delta := range elfDeltas {
+				nextElf := Elf{Point{elf.point.row + delta.row, elf.point.col + delta.col}, elf.stage}
+
+				if validElf(nextElf.point, fieldHeight, fieldWidth, blizzardMap, wallMap) {
+					if reachedFinishPoint(nextElf.point, fieldHeight, fieldWidth) {
+						if withReturn {
+							if elf.stage == SECOND_FORWARD {
+								return minute
+							} else {
+								nextElf.stage = BACK
+							}
+						} else {
+							if elf.stage == FIRST_FORWARD {
+								return minute
+							}
+						}
+					} else if reachedStartPoint(nextElf.point) && elf.stage == BACK {
+						nextElf.stage = SECOND_FORWARD
+					}
+
+					nextElves[nextElf] = true
+				}
+			}
+		}
+
+		minute++
+		elves = nextElves
 	}
 }
 
 func main() {
-	blizzards, fieldHeight, fieldWidth := getInputData()
+	blizzards, wallMap, fieldHeight, fieldWidth := getInputData()
 
-	// for i := 0; i < 6; i++ {
-	// 	printField(blizzards, fieldHeight, fieldWidth)
-	// 	blizzards = moveBlizzards(blizzards, fieldHeight, fieldWidth)
-	// 	fmt.Println()
-	// }
-
-	fmt.Println(bfs(blizzards, fieldHeight, fieldWidth))
+	fmt.Println("Part 1 solution is", findPath(blizzards, wallMap, fieldHeight, fieldWidth, false))
+	fmt.Println("Part 2 solution is", findPath(blizzards, wallMap, fieldHeight, fieldWidth, true))
 }

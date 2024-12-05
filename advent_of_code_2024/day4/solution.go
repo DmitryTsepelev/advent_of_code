46e561ade60c9d6be24102705fc3d647d759789d
DmitryTsepelev <dmitry.a.tsepelev@gmail.com>
2024-12-05 17:36:25 +0300

    fix day4

--- advent_of_code_2024/day4/solution.go
+++ advent_of_code_2024/day4/solution.go
@@ -20,7 +20,7 @@ func getInputData() [][]byte {
 	return input
 }
 
-var deltas = [][]int{
+var deltas1 = [][]int{
 	{-1, -1}, {-1, 0}, {-1, 1},
 	{0, -1}, {0, 1},
 	{1, -1}, {1, 0}, {1, 1},
@@ -31,66 +31,7 @@ func validPoint(input [][]byte, nextPoint []int) bool {
 
 	return row >= 0 && row < len(input) &&
 		col >= 0 && col < len(input[0])
-
-	// if row >= 0 && row < len(input) &&
-	// 	col >= 0 && col < len(input[0]) {
-	// 	// fmt.Println(row, col, string(input[row][col]), string(word[step]))
-	// 	return input[row][col] == word[step]
-	// }
-	// return false
-	//==
-
-	// return row >= 0 && row < len(input) &&
-	// 	col >= 0 && col < len(input[0]) &&
-	// 	input[row][col] == word[step]
 }
-
-// func bfs(input [][]byte, row, col int) int {
-// 	fmt.Println("BFS", row, col)
-// 	word := "XMAS"
-
-// 	queue := [][]int{{row, col}}
-
-// 	for step := 0; step <= 3; step++ {
-// 		fmt.Println("step", step)
-// 		for _, p := range queue {
-// 			fmt.Println(string(input[p[0]][p[1]]))
-// 		}
-// 		// fmt.Println(queue)
-// 		nextQueue := make(map[[2]int]bool)
-
-// 		for _, point := range queue {
-// 			for _, delta := range deltas {
-// 				nextPoint := []int{point[0] + delta[0], point[1] + delta[1]}
-
-// 				if step < 3 && validPoint(input, nextPoint, word, step+1) && input[nextPoint[0]][nextPoint[1]] == word[step+1] {
-// 					// nextQueue = append(nextQueue, nextPoint)
-// 					nextQueue[[2]int{nextPoint[0], nextPoint[1]}] = true
-// 				}
-// 			}
-// 		}
-
-// 		if step == 3 {
-// 			break
-// 		}
-
-// 		// queue = nextQueue
-
-// 		queue = [][]int{}
-// 		for p := range nextQueue {
-// 			queue = append(queue, []int{p[0], p[1]})
-// 		}
-// 	}
-// 	// fmt.Println("=--", queue)
-
-// 	uniquePoints := make(map[[2]int]bool)
-// 	for _, p := range queue {
-// 		uniquePoints[[2]int{p[0], p[1]}] = true
-// 	}
-// 	fmt.Println("adding", len(uniquePoints))
-
-// 	return len(uniquePoints)
-// }
 
 func part1(input [][]byte) int {
 	var count int
@@ -98,24 +39,13 @@ func part1(input [][]byte) int {
 
 	for i := 0; i < len(input); i++ {
 		for j := 0; j < len(input[0]); j++ {
-			// if input[i][j] == 'X' {
-			// 	// count += bfs(input, i, j)
-
-			// }
-
-			if input[i][j] != 'X' {
-				continue
-			}
-
-			for _, delta := range deltas {
+			for _, delta := range deltas1 {
 				row, col := i, j
 				validWord := true
 
 				for step := 0; step <= 3; step++ {
-					fmt.Println("row", row, "col", col)
 					if !validPoint(input, []int{row, col}) || word[step] != input[row][col] {
 						validWord = false
-						fmt.Println("invalid!")
 						break
 					}
 
@@ -124,7 +54,35 @@ func part1(input [][]byte) int {
 				}
 
 				if validWord {
-					fmt.Println("valid")
+					count++
+				}
+			}
+		}
+	}
+
+	return count
+}
+
+var deltas2 = [][]int{{-1, -1}, {-1, 1}, {1, -1}, {1, 1}}
+
+func part2(input [][]byte) int {
+	var count int
+
+	for i := 1; i < len(input)-1; i++ {
+		for j := 1; j < len(input[0])-1; j++ {
+			if input[i][j] == 'A' {
+				xRunes := []byte{}
+
+				for _, delta := range deltas2 {
+					xRunes = append(xRunes, input[i+delta[0]][j+delta[1]])
+				}
+
+				xCorners := string(xRunes)
+
+				if xCorners == "MSMS" ||
+					xCorners == "MMSS" ||
+					xCorners == "SMSM" ||
+					xCorners == "SSMM" {
 					count++
 				}
 			}
@@ -137,5 +95,6 @@ func part1(input [][]byte) int {
 func main() {
 	input := getInputData()
 
-	fmt.Println(part1(input))
+	fmt.Println("Part 1 solution is", part1(input))
+	fmt.Println("Part 2 solution is", part2(input))
 }
